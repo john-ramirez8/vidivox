a30ecc7c84682a6760bcc9eabe0534f3ed12ba54
Michael <jkpop@hotmail.co.nz>
2015-09-20 01:18:15 +1200

    fully botched add audio layout

--- AS3/src/prototype/AddAudio.java
+++ AS3/src/prototype/AddAudio.java
@@ -1,6 +1,7 @@
 package prototype;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -25,9 +26,9 @@ public class AddAudio extends JFrame {
 		setLocation(100, 100);
 		setSize(284, 111);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new BorderLayout(30, 0));
 		setTitle("Add Audio");
 		
 		//Component declarations
@@ -42,8 +43,9 @@ public class AddAudio extends JFrame {
 		lblEnterTheName.setBounds(10, 11, 264, 25);
 		
 		//Setting up the textField
-		textField.setBounds(10, 47, 264, 20);
+		//textField.setBounds(10, 47, 264, 20);
 		textField.setColumns(10);
+		textField.setPreferredSize(new Dimension(264,20));
 		
 		//Setting up the confirm button
 		btnConfirm.addActionListener(new ActionListener() {
@@ -51,7 +53,8 @@ public class AddAudio extends JFrame {
 				setVisible(false);
 			}
 		});
-		btnConfirm.setBounds(10, 78, 89, 23);
+		//btnConfirm.setBounds(10, 78, 89, 23);
+		btnConfirm.setPreferredSize(new Dimension(89,23));
 		
 		//Setting up the cancel button
 		btnCancel.addActionListener(new ActionListener() {
@@ -59,12 +62,13 @@ public class AddAudio extends JFrame {
 				setVisible(false);
 			}
 		});
-		btnCancel.setBounds(185, 78, 89, 23);
+		//btnCancel.setBounds(185, 78, 89, 23);
+		btnConfirm.setPreferredSize(new Dimension(89,23));
 		
 		//Adding all components to the panel
-		contentPane.add(lblEnterTheName);
-		contentPane.add(textField);
-		contentPane.add(btnConfirm);
-		contentPane.add(btnCancel);
+		contentPane.add(lblEnterTheName, BorderLayout.NORTH);
+		contentPane.add(textField, BorderLayout.CENTER);
+		contentPane.add(btnConfirm, BorderLayout.SOUTH);
+		contentPane.add(btnCancel, BorderLayout.SOUTH);
 	}
 }
