df6e14938c3364947a5f1c66e14d259f40c9e204
Michael <jkpop@hotmail.co.nz>
2015-09-21 07:27:07 +1200

    -Set look and feel and added native discover for vlc library

--- AS3/src/prototype/Main.java
+++ AS3/src/prototype/Main.java
@@ -1,10 +1,21 @@
 package prototype;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import uk.co.caprica.vlcj.discovery.NativeDiscovery;
 
 //This is the class responsible for launching the application
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		
+		//Sets look and feel to the users Systems look and feel
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		
+		//Finding the users vlcj library
+		new NativeDiscovery().discover();
+		
 		//Declares main menu and launches it
         SwingUtilities.invokeLater(new Runnable() {
             @Override
