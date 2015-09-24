4117ba1f3c3c63b1742eec2fd09aea2d4ee5c6f8
Michael <jkpop@hotmail.co.nz>
2015-09-24 20:18:05 +1200

    Commented FileOpener class and also added recursive call to openFile() function

--- AS3/src/helpers/FileOpener.java
+++ AS3/src/helpers/FileOpener.java
@@ -7,16 +7,16 @@ import java.io.IOException;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-
+//This class is a helper class, It is used to open and save files
 public class FileOpener {
 	private String desiredExtension;
 	private JFileChooser fc = new JFileChooser();
-	private FileWriter fw;
 	private File openedFile;
 	private File savedFile;
 	private JFrame parentFrame;
 	private String errMessage;
 
+	//The constructor sets the default error message and which frame is the parent frame of the file chooser
 	public FileOpener(String extension, JFrame parentFrame) {
 		this.parentFrame = parentFrame;
 		this.desiredExtension = extension;
@@ -26,7 +26,9 @@ public class FileOpener {
 			errMessage = "Please specify a file of .mp3 type";
 		}
 	}
-
+	
+	//This function returns a variable of File type. It opens a file chooser and allows them to choose a file
+	//If the file is of incorrect file type then the function calls itself recursively to allow the user to try again
 	public File openFile() {
 		int result = fc.showOpenDialog(null);
 		if (result == JFileChooser.APPROVE_OPTION) {
@@ -34,7 +36,7 @@ public class FileOpener {
 			String extension = openedFile.toString().substring(openedFile.toString().length() - 4);
 			if (extension.equals(desiredExtension) == false) {
 				JOptionPane.showMessageDialog(parentFrame, errMessage, "File type Error", JOptionPane.ERROR_MESSAGE);
-				return null;
+				return openFile();
 			} else {
 				return openedFile;
 			}
@@ -42,6 +44,9 @@ public class FileOpener {
 			return null;
 		}
 	}
+	
+	//This function returns a variable of String type of the path of the file chosen by the user. If the file is of 
+	//incorrect file type then the function calls itself recursively to allow the user to try again.
 	
 	public String saveFile() {
 		int result = fc.showDialog(null, "Save");
